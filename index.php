d03038791311e0c10e058b6eeb1d487332ebddf5
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-05-30 01:57:41 +0000

    Finished website

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <!-- Metadata -->
     <meta charset="utf-8">
-    <meta name="description" content="The factorial of a positive integer">
+    <meta name="description" content="Inverted, flipped, right triangle of stars">
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="Julien L.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,7 +20,7 @@
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- Title -->
-    <title>The factorial of a positive integer</title>
+    <title>Inverted, flipped, right triangle of stars</title>
   </head>
   <body>
     <!-- Google's MDL -->
@@ -29,103 +29,23 @@
     <div class = "mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <!--Header and Text-->
     <center>
-      <?php echo "<h1>The factorial of a positive integer</h1>" ?>
+      <?php echo "<h1>Inverted, flipped, right triangle of stars</h1>" ?>
       <?php echo "<h4>By: Julien Lamoureux</h4>" ?>
       <?php echo "<h4>Course: ICS20 Programming</h4>" ?>
       <br>
       <!--User Input-->
       <?php echo "<h3>Please enter the following:</h3>" ?>
       <form action="./results.php" method="post" target="result">
-        <label for="first-int">Enter a postive integer:</label>
-          <input type="number" id="first-int" step="1" placeholder="Eg. 4" name="first-int">
+        <label for="height">Enter a height for your triangle</label>
+          <input type="number" id="height" step="1" placeholder="Eg. 4" name="height">
            <br><br>
-        <input type="submit" value="Calculate Product"><br><br>	
+        <input type="submit" value="Show Triangle"><br><br>	
       </form>
+      <div style="text-align:center"> 
       <!--Display Statement for Museum Admission-->
       <iframe id="result" name="result">
- <?php
-   // initialize results to an empty string
-  $results = "";
-
-  // initialize counter to 1
-  $counter = 1;
-
-  // initialize star to empty string
-  $star = "";
-
-  // get userInteger from user input
-  $userInteger = $_POST["first-int"];
-
-
-  // initialize resultsUpsideDown to an empty string
-  $resultsUpsideDown= "";
-
-  // initialize counterUpsideDown to an empty string
-  $starUpsideDown = "";
-
-// if userInteger is an empty string or is less than or equal to 0, then tell the user to input a postive integer 
-if(($userInteger=="") || ($userInteger <= 0)) {
-  $results = "Please enter a positive integer greater than 0.";
-}
-
-//otherwise, use a do...while loop to display an upright, right, and flipped triangle, and use a for...loop to display an inverted, right, flipped triangle
-else {
-  do {
-       if ($counter % 5 == 0) {
-
-         //every 5 lines, repeat the planet symbol "o" the number of times that the counter is equal to.
-         $star= str_repeat("o",$counter);
-
-         // add a line break
-          $results= $results.$star."<br>";
-
-         //increment the counter
-          $counter++;
-       }
-
-
-       //otherwise, if the counter is not a multiple of 5, reapt the star symbol the number of times that the counter is equal to.
-       else if ($counter % 5 != 0) {
-         //every 5 lines, repeat the star symbol the number of times that the counter is equal to.
-          $star = str_repeat("*",$counter);
-
-          // add a line break
-          $results = $results.$star."<br>";
-
-         //increment the counter
-          $counter++;
-        }
-     } while ($counter <= $userInteger);
-
-     for ($counterUpsideDown=$userInteger;$counterUpsideDown >= 0; $counterUpsideDown--) {
-
-       
-       if ($counterUpsideDown % 5 == 0) {
-
-          //every 5 lines, repeat the planet symbol "o" the number of times that the counter is equal to.
-        $starUpsideDown= str_repeat("o",$counterUpsideDown);
-
-        // add a line break
-        $resultsUpsideDown= $resultsUpsideDown.$starUpsideDown."<br>";
-       }
-        else if ($counterUpsideDown % 5 != 0) {
-
-          //every 5 lines, repeat the star symbol the number of times that the counter is equal to.
-          $starUpsideDown = str_repeat("*",$counterUpsideDown);
-
-         // add a line break
-      $resultsUpsideDown= $resultsUpsideDown.$starUpsideDown."<br>";
-      }
-     }
-  
-}
-
-  //display results
-  echo $results.$resultsUpsideDown;
-    // define the resultsUpsideDown statement
-  
-?>
     </iframe>
+      </div>
       <!-- MDL Progress Bar with Indeterminate Progress -->
       <div id="p2" class="mdl-progress mdl-js-progress mdl-progress__indeterminate"></div>
     </center>
--- results.php
+++ results.php
@@ -1,71 +1,43 @@
-
 <?php
-   // initialize numbers to an empty string
-  $results = "";
 
-  // initialize counter to 1
-  $counter = 1;
-
-  // initialize star to empty string
+  // initialize star to an empty string
   $star = "";
 
-  // get userInteger from user input
-  $userInteger = $_POST["first-int"];
+  // initialize counter to 1
+  $counter = 0;
 
+    // get height from the user
+    $height = $_POST["height"];
 
-  // initialize resultsUpsideDown to an empty string
-  $resultsUpsideDown = "";
-
-  // initialize counterUpsideDown to an empty string
-  $starUpsideDown = "";
-
-// if userInteger is an empty string or is less than or equal to 0, then tell the user to input a postive integer
-if (($userInteger == "") || ($userInteger <= 0)) {
-    $results = "Please enter a positive integer greater than 0.";
+    // if userInteger is an empty string or is less than or equal to 0, then tell the user to input a positive integer
+    if (($height == "") || ($height <= 0)) {
+    echo "Please enter a positive integer greater than 0.";
 }
-else {
-  //otherwise, use a do...while loop to display an upright, right, and flipped triangle
-    do {
-        if ($counter % 5 == 0) {
-            //every 5 lines, repeat the planet symbol "o" the number of times that the counter is equal to.
-            $star = str_repeat("o", $counter);
 
-            // add a line break
-            $results = $results . $star . "<br>";
+    //otherwise, display an upright, right, and flipped triangle
+    else{
 
-            //increment the counter
+      //use a for loop for the spacing while coutnerSpacing is less than the difference of the height and counterSpace
+    for($counterSpace = 0; $counterSpace < $height; $counterSpace++) {
+        for($counterSpacing = 1; $counterSpacing < $height - $counterSpace; $counterSpacing++) {
+          
+          // print spaces
+            echo "&nbsp;&nbsp;";
+        }
+
+      //use a do...while loop for printing the stars, while the counter is less than or equal to counterSpace
+      do {
+
+           // calculate amount of stars to displau
+           $star= str_repeat("*", $counter+1);
+
+            // increment the counter
             $counter++;
-        }
-       //otherwise, repeat the star symbol the number of times that the counter is equal to.
-        else if ($counter % 5 != 0) {
-            //every 5 lines, repeat the star symbol the number of times that the counter is equal to.
-            $star = str_repeat("*",$counter);
+        } while ($counter <= $counterSpace); 
 
-            // add a line break
-            $results = $results . $star . "<br>";
-
-            //increment the counter
-            $counter++;
-         }
-    } while ($counter <= $userInteger);
-    //use a for...loop to display an inverted, right, flipped triangle
-    for ($counterUpsideDown = $userInteger;$counterUpsideDown >= 0; $counterUpsideDown--) { 
-        if ($counterUpsideDown % 5 == 0) {
-           //every 5 lines, repeat the planet symbol "o" the number of times that the counter is equal to.
-            $starUpsideDown = str_repeat("o",$counterUpsideDown);
-
-            // add a line break
-            $resultsUpsideDown = $resultsUpsideDown . $starUpsideDown . "<br>";
-        }
-        elseif ($counterUpsideDown % 5 != 0) {
-            //every 5 lines, repeat the star symbol the number of times that the counter is equal to.
-            $starUpsideDown = str_repeat("*",$counterUpsideDown);
-
-            // add a line break
-            $resultsUpsideDown = $resultsUpsideDown . $starUpsideDown . "<br>";
-        }
+      //display results
+      echo $star;
+      echo "<br>";        
     }
-}
-
-  //display results
-  echo $results . $resultsUpsideDown;
+    }
+?>
